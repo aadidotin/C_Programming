e8b5e41036ea51b1e10a2c208ffc7cb174b11f2d
aadi-14 <adisharma14500@gmail.com>
2024-06-01 18:51:35 +0530

    Taking input from use

---
+++ Basics/taking_input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    // two sum
+    int a, b;
+    printf("Enter First Number for a :- ");
+    scanf("%d", &a);
+
+    printf("Enter Second Number for b :- ");
+    scanf("%d", &b);
+
+    int sum = a + b;
+    printf("Your total is :- %d", sum);
+}
