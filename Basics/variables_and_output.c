e21d0aa92f5f6a138dc3a84693d32e354488f92c
aadi-14 <adisharma14500@gmail.com>
2024-06-01 18:44:50 +0530

    Variables and Response Output

---
+++ Basics/variables_and_output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    float b = 2.01;
+    char c = 'A';
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int x = 10, y = 20, z = 30;
+
+    printf("%d \n", arr[1]);
+    printf("This is the sum of X, Y and Z %d \n", x + y + z);
+
+    return 0;
+}
