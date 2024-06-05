84f20af0491b44f236c83b6f98894a582b4eb8fa
aadi-14 <adisharma14500@gmail.com>
2024-06-05 19:25:07 +0530

    way to print a string in C

--- 01_Basics/variables_and_output.c
+++ 01_Basics/variables_and_output.c
@@ -23,11 +23,13 @@ int main()
     float cost_per_item = 9.99;
     float total_cost = items * cost_per_item;
     char currency = '$';
+    char name[] = "Aditya Sharma";
 
     // Print variables
     printf("Number of items: %d\n", items);
     printf("Cost per item: %.2f %c\n", cost_per_item, currency);
     printf("Total cost = %.2f %c\n", total_cost, currency);
+    printf("Name = %s\n", name);
 
     return 0;
 }
